29e205aafa95bbf56ceacf0a7cf76c1daf17e96d
luska026 <lucas.sales@usp.br>
2022-10-13 08:31:02 -0300

    working again

--- all/flowers/projeto_flores/NN_flowers.py
+++ all/flowers/projeto_flores/NN_flowers.py
@@ -15,27 +15,45 @@ data = [[5.1, 3.5, 1.4, 0.2, 'Iris-setosa'],
         [4.4, 2.9, 1.4, 0.2, 'Iris-setosa'],
         [4.9, 3.1, 1.5, 0.1, 'Iris-setosa']]
 
+# batch de 10 elementos para teste
+data_ent = [[5.1, 3.5, 1.4, 0.2],
+            [4.9, 3.0, 1.4, 0.2],
+            [4.7, 3.2, 1.3, 0.2],
+            [4.6, 3.1, 1.5, 0.2],
+            [5.0, 3.6, 1.4, 0.2],
+            [5.4, 3.9, 1.7, 0.4],
+            [4.6, 3.4, 1.4, 0.3],
+            [5.0, 3.4, 1.5, 0.2],
+            [4.4, 2.9, 1.4, 0.2],
+            [4.9, 3.1, 1.5, 0.1]]
+
 data = np.array(data)
-print(data)
+data_ent = np.array(data_ent)
 
 
 class NN():
 
     def __init__(self, shape):
-        n_features, n_size = shape
-        n_neurons=4
-        self.weights = np.random.randn(n_features, n_neurons) # cria 4 neurônios com "n_features" de valores para multiplicar cada entrada
-        self.weights_2 = np.random.randn(n_neurons, n_neurons)# cria 4 neurônios com "n_features" de valores para multiplicar cada entrada
-        # self.bias = np.zeros(n_features, n_size)
+        batches, n_features = np.shape(shape)
+        n_neurons = 4
+
+        self.weights = np.random.randn(n_features, n_neurons)
+        self.bias = np.zeros(n_neurons)
+        # cria 4 neurônios com "n_features" de valores para multiplicar cada entrada
+
+        self.weights_2 = 0.10*np.random.randn(n_neurons, n_neurons)
+        self.bias_2 = np.zeros(n_neurons)
+        # cria 4 neurônios com "n_features" de valores para multiplicar cada entrada
 
     def feedForward(self, X_train):
-        output = np.dot(self.weights, X_train)
-        output_2 = np.dot(self.weights, output)
+        output = np.dot(X_train, self.weights) + self.bias
+        output_2 = np.dot(output, self.weights_2) + self.bias_2
         return output_2
 
 
-# print(data[1].T)
-# layer_1 = NN(np.shape(data))
+flowers = NN(data_ent) # entra com a batch pra definir os parâmetros da rede
 
-# result = layer_1.feedForward(data[1].T)
-# print(result)
+for i in range(10):
+    print("\n")
+    result = flowers.feedForward(data_ent) #faz o processamento da entrada de data_ent e produz uma saída de mesma dimensão da batch
+    print(pd.DataFrame(result))
