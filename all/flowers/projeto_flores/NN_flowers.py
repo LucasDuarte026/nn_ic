4e58e44f8924ff4311fe42269c139b2257fe1ebe
luska026 <lucas.sales@usp.br>
2022-10-14 21:42:12 -0300

    Atualização do processamento de entradas

--- all/flowers/projeto_flores/NN_flowers.py
+++ all/flowers/projeto_flores/NN_flowers.py
@@ -1,34 +1,22 @@
-from email import feedparser
-import pandas as pd
 import numpy as np
+import pandas as pd
 
-np.random.seed(0)
-
-data = [[5.1, 3.5, 1.4, 0.2, 'Iris-setosa'],
-        [4.9, 3.0, 1.4, 0.2, 'Iris-setosa'],
-        [4.7, 3.2, 1.3, 0.2, 'Iris-setosa'],
-        [4.6, 3.1, 1.5, 0.2, 'Iris-setosa'],
-        [5.0, 3.6, 1.4, 0.2, 'Iris-setosa'],
-        [5.4, 3.9, 1.7, 0.4, 'Iris-setosa'],
-        [4.6, 3.4, 1.4, 0.3, 'Iris-setosa'],
-        [5.0, 3.4, 1.5, 0.2, 'Iris-setosa'],
-        [4.4, 2.9, 1.4, 0.2, 'Iris-setosa'],
-        [4.9, 3.1, 1.5, 0.1, 'Iris-setosa']]
+data = pd.read_csv('./data.txt')
+data_test = [[5.1, 3.5, 1.4, 0.2, 'Iris-setosa'],
+             [4.9, 3.0, 1.4, 0.2, 'Iris-setosa'],
+             [4.7, 3.2, 1.3, 0.2, 'Iris-setosa'],
+             [4.6, 3.1, 1.5, 0.2, 'Iris-setosa'],
+             [5.0, 3.6, 1.4, 0.2, 'Iris-setosa'],
+             [5.4, 3.9, 1.7, 0.4, 'Iris-setosa'],
+             [4.6, 3.4, 1.4, 0.3, 'Iris-setosa'],
+             [5.0, 3.4, 1.5, 0.2, 'Iris-setosa'],
+             [4.4, 2.9, 1.4, 0.2, 'Iris-setosa'],
+             [4.9, 3.1, 1.5, 0.1, 'Iris-setosa']]
 
 # batch de 10 elementos para teste
-data_ent = [[5.1, 3.5, 1.4, 0.2],
-            [4.9, 3.0, 1.4, 0.2],
-            [4.7, 3.2, 1.3, 0.2],
-            [4.6, 3.1, 1.5, 0.2],
-            [5.0, 3.6, 1.4, 0.2],
-            [5.4, 3.9, 1.7, 0.4],
-            [4.6, 3.4, 1.4, 0.3],
-            [5.0, 3.4, 1.5, 0.2],
-            [4.4, 2.9, 1.4, 0.2],
-            [4.9, 3.1, 1.5, 0.1]]
-
-data = np.array(data)
-data_ent = np.array(data_ent)
+data_X = pd.DataFrame(np.array(data).T[:4].T, dtype='float')
+data_Y = pd.DataFrame(data).T[4:].T
+print(data_Y)
 
 
 class NN():
@@ -36,6 +24,7 @@ class NN():
     def __init__(self, shape):
         batches, n_features = np.shape(shape)
         n_neurons = 4
+        np.random.seed(0)
 
         self.weights = np.random.randn(n_features, n_neurons)
         self.bias = np.zeros(n_neurons)
@@ -51,9 +40,21 @@ class NN():
         return output_2
 
 
-flowers = NN(data_ent) # entra com a batch pra definir os parâmetros da rede
+# entra com a batch pra definir os parâmetros da rede
+flowers = NN(data_X)
 
-for i in range(10):
-    print("\n")
-    result = flowers.feedForward(data_ent) #faz o processamento da entrada de data_ent e produz uma saída de mesma dimensão da batch
-    print(pd.DataFrame(result))
+result = flowers.feedForward(data_X)
+print(pd.DataFrame(result))
+
+# weights = pd.DataFrame(np.random.randn(10, 4).T)
+# print(data_X)
+# print(weights)
+# result = np.dot(data_X, weights).T
+
+
+# for i in range(10):
+#     print("\n")
+
+#     # faz o processamento da entrada de data_X e produz uma saída de mesma dimensão da batch
+#     result = flowers.feedForward(data_X)
+#     print(pd.DataFrame(result))
