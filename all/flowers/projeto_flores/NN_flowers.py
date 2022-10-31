03d4bf1f9d3d764350fcce83b334f4f213e6c3f1
luska026 <lucas.sales@usp.br>
2022-10-30 22:49:19 -0300

    implementado a mudança dos pesos da 3ª camada

--- all/flowers/projeto_flores/NN_flowers.py
+++ all/flowers/projeto_flores/NN_flowers.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 # deixar o argumento da função abaixo caso queira randomizar os valores de entrada
 np.random.seed(200)
-BATCH = 40
-EPOCHS = 100
-LEARN_RATE = 0.3  # constante de aprendizado
+BATCH = 2
+EPOCHS = 1000
+LEARN_RATE = 0.1  # constante de aprendizado
 data = pd.read_csv('./data.txt')
 
 
@@ -14,7 +14,6 @@ data = pd.read_csv('./data.txt')
 
 data_X = np.array(data).T[:4].T.astype(float)
 data_Y_text = np.array(pd.DataFrame(data).T[4:].T)
-
 
 
 ''' train_Y gerado a partir do banco de dados  '''
@@ -26,12 +25,12 @@ for i in range(len(data_Y_text)):
         data_Y = np.append(data_Y, [[0, 1, 0]], axis=0)
     elif data_Y_text[i] == 'Iris-virginica':
         data_Y = np.append(data_Y, [[0, 0, 1]], axis=0)
-data_Y_test = np.array(pd.DataFrame(data_Y)[:10])
 
 #############################################################################################
 
-
 ''' rede neural usada '''
+
+
 class NN():
 
     def __init__(self, train_model):
@@ -39,7 +38,7 @@ class NN():
         n_neurons = 4
 
         self.weights = 0.10*np.random.randn(n_features, n_neurons)
-        self.bias = np.zeros(n_neurons)
+        self.bias_1 = np.zeros(n_neurons)
         ''' cria 4 neurônios com "n_features" de valores para multiplicar cada entrada dos dados iniciais'''
 
         self.weights_2 = 0.10*np.random.randn(n_neurons, n_neurons)
@@ -47,7 +46,7 @@ class NN():
         ''' cria 4 neurônios com "n_features" de valores para multiplicar cada entrada da primeira camada'''
 
         self.weights_3 = 0.10*np.random.randn(n_neurons, 3)
-        self.bias_3 = np.zeros(3)
+        self.bias_output = np.zeros(3)
         ''' cria 3 neurônios com "n_features" de valores para multiplicar cada entrada da segunda camada'''
 
     def ReLU(self, input):
@@ -70,11 +69,11 @@ class NN():
         return a*(1-a)
 
     def feed_Forward(self, X_train):
-        self.output_1 = self.ReLU(np.dot(X_train, self.weights) + self.bias)
+        self.output_1 = self.ReLU(np.dot(X_train, self.weights) + self.bias_1)
         self.output_2 = self.ReLU(
             np.dot(self.output_1, self.weights_2) + self.bias_2)
         result = self.sigmoid(
-            np.dot(self.output_2, self.weights_3) + self.bias_3)
+            np.dot(self.output_2, self.weights_3) + self.bias_output)
         return result
 
     def error(self, X_train, Y_train):
@@ -84,57 +83,51 @@ class NN():
         return error
 
     def back_Propagation(self, X_train, Y_train):
-        error = self.error(X_train, Y_train)
-        print("error\n", error)
-        
-        
+        result = self.feed_Forward(X_train)
+        erro = result - Y_train
+        for neuron in range(len(self.weights_3)):
+            for idx in range(len(self.weights_3[neuron])):
+                delta_w = LEARN_RATE*erro[0][idx]
+                self.bias_output[idx] = delta_w
+
+                delta_w *= self.output_2[0][idx]
+                self.weights_3[neuron][idx] -= delta_w
+
+
 #############################################################################################
 
-
 ''' entra com a batch pra definir os parâmetros da rede '''
-flowers = NN(data_X[:BATCH])
-result = flowers.feed_Forward(data_X[:BATCH])
-flowers.back_Propagation(data_X[:BATCH],data_Y[:BATCH])
+flowers = NN(data_X)
+result = flowers.feed_Forward(data_X)
+
+
+# debug methods
 
 # print("weights_1\n", pd.DataFrame(flowers.weights))
 # print("weights_2\n", pd.DataFrame(flowers.weights_2))
-# print("weights_3\n", pd.DataFrame(flowers.weights_3))
+# print("\nweights_3\n", pd.DataFrame(flowers.weights_3))
 # print("output_1\n", pd.DataFrame(flowers.output_1))
-# print("output_2\n", pd.DataFrame(flowers.output_2))
-print("result\n", pd.DataFrame(result))
-print("data\n", pd.DataFrame(data_Y[:BATCH]))
+# print("\noutput_2\n", pd.DataFrame(flowers.output_2))
+# print("\nbias_output\n", pd.DataFrame(flowers.bias_output))
+# print("\nresult\n", pd.DataFrame(result))
+# print("\ndata\n", pd.DataFrame(data_Y))
 
+init_diff = result-data_Y
 
 ''' treinando e alimentando a rede com os dados '''
-
-new_weights = np.empty((0, flowers.weights_3.shape[1]))
-
-# print("error\n", pd.DataFrame(error))
-# print("error\n", pd.DataFrame(error-result))
-
-
-'''
-print("antes:\n", pd.DataFrame(flowers.weights_3))
-for idx in range(EPOCHS):
-    new_weights = np.empty((0, 3))
-    for i in range(len(flowers.weights_3)):
-        ins_arr = [0, 0, 0]
-        error = flowers.back_Propagation(data_X[:BATCH], data_Y[:BATCH])
-        result = flowers.feed_Forward(data_X[:BATCH])
-        sig_deriv_arr = flowers.sigmoid_deriv(result)
-
-        for j in range(len(flowers.weights_3[i])):
-            new_value = flowers.weights_3[i][j]
-            changing = (error[j]-result[i][0]) * flowers.output_2[i][j]*sig_deriv_arr[i][j]
-            new_value += LEARN_RATE * changing
-            ins_arr[j] = new_value
-        new_weights = np.append(new_weights, [ins_arr], axis=0)
-
-    # print("\nnew_weights\n", pd.DataFrame(new_weights))
-    flowers.weights_3 = new_weights
-    result = flowers.feed_Forward(data_X[:BATCH])
-    # print("new_result\n", pd.DataFrame(new_result))
-    # print("data_Y[:BATCH]\n", pd.DataFrame(data_Y[:BATCH]))
-print("dps:\n", pd.DataFrame(flowers.weights_3))
-print("result:\n", pd.DataFrame(result))
-'''
+for i in range(EPOCHS):
+    if (i//10):
+        print('.',end='')
+    for idx in range(len(data_X)//BATCH):
+        part = idx*BATCH
+        print(part)
+        flowers.back_Propagation(
+            data_X[part:part+BATCH], data_Y[part:part+BATCH])
+        result = flowers.feed_Forward(data_X[part:part+BATCH])
+        diff = result-data_Y[part:part+BATCH]
+        
+print()
+result = flowers.feed_Forward(data_X)
+final_diff = result-data_Y
+print('init_diff: ', np.mean(init_diff, axis=0))
+print('final_diff: ', np.mean(final_diff, axis=0))
